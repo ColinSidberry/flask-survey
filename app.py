a452618957f23bb78e37c005c916b5eb90062c64
Colin Sidberry <colinsidberry@colins-mbp.lan>
2021-09-09 17:34:09 -0700

    added session management and refactored based on code review

--- app.py
+++ app.py
@@ -8,44 +8,61 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY = "responses"
 
 @app.get("/")
 def render_survey():
-    """Renders the survey starting page and clears previous responses."""
-    responses.clear()
-    survey_title = survey.title
-    survey_instructions = survey.instructions
-    #could also just pass in servey and refer to it in html
-    return render_template("survey_start.html", 
-                            survey_title = survey_title, 
-                            survey_instructions = survey_instructions)
+    """Renders the survey starting page.
+    """
+    return render_template("survey_start.html",
+                           survey=survey)
+
 
 @app.post("/begin")
 def go_to_questions():
-    """Goes to first survey question."""
+    """
+    Better:
+    Sets up empty list of responses and redirects to the first question
+
+    Original:
+    Goes to first survey question. and populates the session object 
+    at the key of "responses" to an empty list."""
+
+    session[RESPONSES_KEY] = []
     return redirect("/questions/0")
+
 
 @app.get("/questions/<int:ques_num>")
 def ask_questions(ques_num):
-    """Displays survey question and choices."""
-    survey_question= survey.questions[ques_num]
-    question_choices= survey_question.choices
-    return render_template("question.html", 
-                            choices = question_choices,
-                            question = survey_question)
+    """Displays survey question and choices. If user tries to answer 
+    survey questions out of order, redirect them to the correct survey question"""
+
+
+    #note: have the guard first
+    if len(session["responses"]) != ques_num:
+        flash("Please answer the questions in order.")
+        return redirect(f"/questions/{len(session['responses'])}")
+    
+    return render_template("question.html",
+                               question=survey.questions[ques_num])
+
 
 @app.post("/answer")
 def go_to_next_question():
-    """Populates response list with survery answers. If all survey questions are answered, route to completion page."""
+    """Populates response list with survery answers. 
+    If all survey questions are answered, route to completion page."""
+
+    responses = session["responses"]
     responses.append(request.form["answer"])
-    if len(responses)<len(survey.questions):
+    session["responses"] = responses
+
+    if len(responses) < len(survey.questions):
         return redirect(f"/questions/{len(responses)}")
     else:
         return redirect("/completion")
+
 
 @app.get("/completion")
 def render_thanks():
     """Renders completions page to thank the user for completing survey."""
     return render_template("completion.html")
-
